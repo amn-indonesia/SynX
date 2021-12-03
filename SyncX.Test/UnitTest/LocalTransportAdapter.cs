55de8e9b3ad26c90ac06fabe8eaff7802d0ee6dc
Harry Saputra Kartono <hskartono@gmail.com>
2021-12-03 14:31:50 +0700

    tambahan local transport dan file

---
+++ SyncX.Test/UnitTest/LocalTransportAdapter.cs
@@ -0,0 +1,65 @@
+﻿using SynX.Core.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynX.Test.UnitTest
+{
+    public class LocalTransportAdapter : Core.ITransportAdapter
+    {
+        public Exception ExceptionData { get; set; }
+
+        public bool DownloadFile(string remoteFileName, string localFileName, SyncConfig syncConfig)
+        {
+            if (!System.IO.File.Exists(remoteFileName)) return false;
+            System.IO.File.Copy(remoteFileName, localFileName, true);
+            return true;
+        }
+
+        public List<string> GetFileList(SyncConfig syncConfig)
+        {
+            var list = System.IO.Directory.GetFiles(syncConfig.RemotePath, "*.xml");
+            return list.ToList();
+        }
+
+        public bool MoveFile(string remoteSourceFileName, string remoteDestinationFileName, SyncConfig syncConfig)
+        {
+            if (!System.IO.File.Exists(remoteSourceFileName)) return false;
+            if (!System.IO.File.Exists(remoteDestinationFileName)) return false;
+            System.IO.File.Copy(remoteSourceFileName, remoteDestinationFileName);
+            return true;
+        }
+
+        public bool MoveToBackup(string remoteFileName, SyncConfig syncConfig)
+        {
+            var destinationFolder = syncConfig.BackupOutPath;
+            if (!System.IO.File.Exists(remoteFileName)) return false;
+            System.IO.File.Copy(remoteFileName, destinationFolder);
+            return true;
+        }
+
+        public string ReadFileContent(string remoteFileName, SyncConfig syncConfig)
+        {
+            if (!System.IO.File.Exists(remoteFileName)) return String.Empty;
+            var content = System.IO.File.ReadAllText(remoteFileName);
+            return content;
+        }
+
+        public bool RemoveFile(string remoteFileName, SyncConfig syncConfig)
+        {
+            if (!System.IO.File.Exists(remoteFileName)) return false;
+            System.IO.File.Delete(remoteFileName);
+            return true;
+        }
+
+        public bool UploadFile(string localFileName, SyncConfig syncConfig)
+        {
+            var destinationFolder = syncConfig.TransportConfig.LocalPath;
+            if (!System.IO.File.Exists(localFileName)) return false;
+            System.IO.File.Copy(localFileName, destinationFolder);
+            return true;
+        }
+    }
+}
---
+++ SyncX.Test/UnitTest/SyncEngineTest.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using SynX.Core;
+using SynX.Core.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SynX.Test.UnitTest
+{
+    public class SyncEngineTest : SyncLogSqliteTest
+    {
+        public SyncEngineTest() : base(
+            new DbContextOptionsBuilder<SyncLogDbContext>()
+            .UseSqlite("Filename=test.db")
+            .Options)
+        {
+
+        }
+
+        [Fact]
+        public async Task SyncSet_Return_IDNo_XMLContentCreated()
+        {
+            using (var context = new SyncLogDbContext(ContextOptions))
+            {
+                var syncLog = new SyncLogService(context);
+                var sync = SyncEngine.CreateInstance("default");
+
+                var payload = new Dictionary<string, object>();
+                payload.Add("TransactionDate", "2021-11-15 14:53:00");
+                payload.Add("Plant", "SAP");
+
+
+                var idNo = await sync.SendSyncSet("unit_test", "UT_RECORD_ID", payload);
+                var log = await context.SyncLogs.Where(e => e.IdNo == idNo).FirstOrDefaultAsync();
+                log.Should().NotBeNull();
+                log.IsSyncOut.Should().BeTrue();
+            }
+        }
+    }
+}
